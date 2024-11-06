ea40f58fb1c2d4d0d529954cf037816bec6e08e4
Sagi Barshai <sagibarshai1@gmail.com>
2024-11-06 19:39:59 +0200

    remove cookie token after signup

--- backend/src/features/auth/sign-up/controller.ts
+++ backend/src/features/auth/sign-up/controller.ts
@@ -19,7 +19,6 @@ export const signUpController = async (req: SignUpRequest, res: Response, next: 
     }
     const { safeUser, verificationToken } = await InsertUserModel({ ...req.body, password: hashedPassword });
 
-    // createTokenAndSetCookie(safeUser, req);
     sendEmailVerification({ id: safeUser.id, to: safeUser.email, token: verificationToken });
 
     res.status(201).send(safeUser);
